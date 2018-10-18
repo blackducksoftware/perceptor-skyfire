fce9c082a81c460cdabd7bdd88083ed009e04bac
Michael Hammer <mike.p.hammer@gmail.com>
2018-10-18 13:53:40 -0400

    Loop to print num services every 3 secs

--- cmd/getkubeservices/getkubeservices.go
+++ cmd/getkubeservices/getkubeservices.go
@@ -1,26 +1,30 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"os"
 	"time"
 
 	kube "github.com/blackducksoftware/perceptor-skyfire/pkg/kube"
+	"github.com/prometheus/client_golang/prometheus"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	//GetServicesMetrics()
-	DumpServices()
+	startServicesMetricsRoutine()
+	//DumpServices()
+}
+
+func startServicesMetricsRoutine() {
+	for i := 0; i < 3; i++ {
+		// Get Metrics
+		GetServicesMetrics()
+		// Sleep
+		time.Sleep(3 * time.Second)
+	}
 }
 
 func GetServicesMetrics() {
-	for {
-		// Get Metrics
-
-		// Sleep
-		time.Sleep(20 * time.Second)
-	}
+	DumpServices()
 }
 
 func DumpServices() {
@@ -44,15 +48,74 @@ func DumpServices() {
 	}
 
 	// Create a map of the Service Dump for JSON formatting
-	dict := map[string]interface{}{
-		"Dump": kubeServicesDump,
-	}
+	//dict := map[string]interface{}{
+	//	"Dump": kubeServicesDump,
+	//}
 
-	bytes, err := json.MarshalIndent(dict, "", "  ")
-	if err != nil {
-		panic(err)
-	}
+	//bytes, err := json.MarshalIndent(dict, "", "  ")
+	//if err != nil {
+	//	panic(err)
+	//}
 
-	fmt.Println(string(bytes))
+	//fmt.Println(string(bytes))
 
+	log.Infof("Number of Services: %d", len(kubeServicesDump.Services))
+
+}
+
+//var linkTypeDurationHistogram *prometheus.HistogramVec
+var problemsGauge *prometheus.GaugeVec
+var errorCounter *prometheus.CounterVec
+var eventCounter *prometheus.CounterVec
+
+/*func recordLinkTypeDuration(linkType LinkType, duration time.Duration) {
+	milliseconds := float64(duration / time.Millisecond)
+	linkTypeDurationHistogram.With(prometheus.Labels{"linkType": linkType.String()}).Observe(milliseconds)
+}*/
+
+func recordReportProblem(name string, count int) {
+	problemsGauge.With(prometheus.Labels{"name": name}).Set(float64(count))
+}
+
+func recordError(name string) {
+	errorCounter.With(prometheus.Labels{"name": name}).Inc()
+}
+
+func recordEvent(name string) {
+	eventCounter.With(prometheus.Labels{"name": name}).Inc()
+}
+
+func init() {
+	/*linkTypeDurationHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: "perceptor",
+		Subsystem: "skyfire",
+		Name:      "hub_api_link_duration",
+		Help:      "durations for hub API calls in milliseconds, grouped by link type",
+		Buckets:   prometheus.ExponentialBuckets(1, 2, 20),
+	}, []string{"linkType"})
+	prometheus.MustRegister(linkTypeDurationHistogram)*/
+
+	problemsGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "perceptor",
+		Subsystem: "skyfire",
+		Name:      "test_issues",
+		Help:      "names and counts for issues discovered in perceptor testing",
+	}, []string{"name"})
+	prometheus.MustRegister(problemsGauge)
+
+	errorCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "perceptor",
+		Subsystem: "skyfire",
+		Name:      "errors",
+		Help:      "internal skyfire errors",
+	}, []string{"name"})
+	prometheus.MustRegister(errorCounter)
+
+	eventCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "perceptor",
+		Subsystem: "skyfire",
+		Name:      "events",
+		Help:      "internal skyfire events",
+	}, []string{"name"})
+	prometheus.MustRegister(eventCounter)
 }
