c74ae49ff6a95ed003654e22e522822b5cc30c60
Matt Fenwick <mfenwick100@gmail.com>
2018-10-31 09:30:20 -0400

    hand off events to delegate

--- pyfire/src/scraper.py
+++ pyfire/src/scraper.py
@@ -1,16 +1,13 @@
-import queue
 import time
 import threading
-from cluster_clients import PerceptorClient, KubeClientWrapper, HubClient
 import metrics
-import json 
 import sys
 import logging 
 
 
 class Scraper(object):
-    def __init__(self, perceptor_client, kube_client, hub_clients, perceptor_pause=30, kube_pause=30, hub_pause=60):
-        self.q = queue.Queue()
+    def __init__(self, delegate, perceptor_client, kube_client, hub_clients, perceptor_pause=30, kube_pause=30, hub_pause=60):
+        self.delegate = delegate
 
         self.perceptor_client = perceptor_client
         self.perceptor_thread = threading.Thread(target=self.perceptor)
@@ -50,28 +47,42 @@ class Scraper(object):
 
     def perceptor(self):
         while self.is_running:
-            perceptor_scrape = self.perceptor_client.get_scrape()
-            self.q.put(perceptor_scrape)
+            scrape = self.perceptor_client.get_scrape()
+            self.delegate.perceptor_dump(scrape)
             logging.debug("got perceptor dump")
             metrics.record_event("perceptorDump")
             time.sleep(self.perceptor_pause)
     
     def kube(self):
         while self.is_running:
-            kube_scrape = self.kube_client.get_scrape()
-            self.q.put(kube_scrape)
+            scrape = self.kube_client.get_scrape()
+            self.delegate.kube_dump(scrape)
             logging.debug("got kube dump")
             metrics.record_event("kubeDump")
             time.sleep(self.kube_pause)
 
     def hub(self):
         while self.is_running:
-            hub_scrape = self.hub_clients[list(self.hub_clients.keys())[0]].get_scrape()
-            self.q.put(hub_scrape)
+            # TODO use one thread per hub
+            for hub_host, hub_client in self.hub_clients.items():
+                scrape = hub_client.get_scrape()
+                self.delegate.hub_dump(hub_host, scrape)
             logging.debug("got hub dump")
             metrics.record_event("hubDump")
             time.sleep(self.hub_pause)
 
+
+class MockDelegate:
+    def __init__(self):
+        import queue
+        self.q = queue.Queue()
+    def perceptor_dump(self, dump):
+        self.q.put(("perceptor", dump))
+    def kube_dump(self, dump):
+        self.q.put(("kube", dump))
+    def hub_dump(self, host, dump):
+        self.q.put(("hub", dump, host))
+    
 
 def reader():
     """
@@ -82,24 +93,36 @@ def reader():
             self.name = name
         def get_scrape(self):
             return {'scrape_type': self.name}
+    
+    delegate = MockDelegate()
     hub_clients = {
         'abc': MockScraper("hubabc"),
         'def': MockScraper("hubdef")
     }
-    s = Scraper(MockScraper("perceptor"), MockScraper("kube"), hub_clients, perceptor_pause=2, kube_pause=3, hub_pause=4)
+    s = Scraper(
+        delegate,
+        MockScraper("perceptor"),
+        MockScraper("kube"), 
+        hub_clients, 
+        perceptor_pause=2, 
+        kube_pause=3, 
+        hub_pause=4)
     s.start()
+
     while True:
-        item = s.q.get()
+        item = delegate.q.get()
         print("got next:", item)
         if item is None:
             break
 #        f(item)
-        s.q.task_done()
+        delegate.q.task_done()
 
 def real_reader(conf):
     """
     Another example not to be used in PDCs!
     """
+    from cluster_clients import PerceptorClient, KubeClientWrapper, HubClient
+
     perceptor_client = PerceptorClient(conf['Perceptor']['Host'])
 
     kube_client = KubeClientWrapper(conf["Skyfire"]["UseInClusterConfig"])
@@ -108,14 +131,16 @@ def real_reader(conf):
     for host in conf["Hub"]["Hosts"]:
         hub_clients[host] = HubClient(host, conf["Hub"]["User"], conf["Hub"]["PasswordEnvVar"])
 
-    s = Scraper(perceptor_client, kube_client, hub_clients, perceptor_pause=15, kube_pause=15, hub_pause=30)
+    delegate = MockDelegate()
+    s = Scraper(delegate, perceptor_client, kube_client, hub_clients, perceptor_pause=15, kube_pause=15, hub_pause=30)
     s.start()
+
     while True:
-        item = s.q.get()
+        item = delegate.q.get()
         print("got next:", item)
         if item is None:
             break
-        s.q.task_done()
+        delegate.q.task_done()
 
 
 if __name__ == "__main__":
@@ -124,6 +149,7 @@ if __name__ == "__main__":
         t = threading.Thread(target=reader)
         t.start()
     else:
+        import json
         config_path = sys.argv[1]
         with open(config_path) as f:
             config_json = json.load(f)
