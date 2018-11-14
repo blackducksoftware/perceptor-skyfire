46f3c43b03b1a2c8a86c86e7737a17274500030a
Michael Hammer <mike.p.hammer@gmail.com>
2018-11-14 01:00:14 -0500

    OpsSight and Hub Coverage Tests

--- pyfire/src/testsuite.py
+++ pyfire/src/testsuite.py
@@ -48,7 +48,9 @@ class TestSuite:
             self.test_results["state"] = "IN_PROGRESS"
             self.test_results["data"]['MockTest1'] = ""
             self.test_results["data"]['MockTest2'] = ""
-            self.test_results["data"]['MockTest3'] = ""
+            self.test_results["data"]['OpsSightRepoCoverage'] = ""
+            self.test_results["data"]['OpsSightPodCoverage'] = ""
+            self.test_results["data"]['HubImageCoverage'] = ""
             self.accessing_data = False
 
             r = self.mock_test()
@@ -65,11 +67,25 @@ class TestSuite:
             self.test_results["data"]['MockTest2'] = r
             self.accessing_data = False
 
-            r = self.mock_test()
+            r = self.opssight_repo_coverage(50)
             while self.accessing_data:
                 pass
             self.accessing_data = True
-            self.test_results["data"]['MockTest3'] = r
+            self.test_results["data"]['OpsSightRepoCoverage'] = r
+            self.accessing_data = False
+
+            r = self.opssight_pod_coverage(90)
+            while self.accessing_data:
+                pass
+            self.accessing_data = True
+            self.test_results["data"]['OpsSightPodCoverage'] = r
+            self.accessing_data = False
+
+            r = self.hub_image_coverage(90)
+            while self.accessing_data:
+                pass
+            self.accessing_data = True
+            self.test_results["data"]['HubImageCoverage'] = r
             self.accessing_data = False
 
             while self.accessing_data:
@@ -87,19 +103,55 @@ class TestSuite:
             self.in_progress = False
 
     def mock_test(self):
-        time.sleep(10)
+        time.sleep(7)
         val = random.randint(0,10)
         if val <= 8:
             return "PASSED"
         else:
             return "FAILED"
 
+    def opssight_repo_coverage(self, policy):
+        dump, err = getSkyfireDump()
+        total_repos = 0
+        total_repos += len(dump["perceptor-kube-report"]["only_kube_repos"])
+        total_repos += len(dump["perceptor-kube-report"]["only_perceptor_repos"])
+        total_repos += len(dump["perceptor-kube-report"]["both_repos"])
+        num_in_both = len(dump["perceptor-kube-report"]["both_repos"])
+        logging.debug("OpsSight Repo Coverage: %f", (num_in_both / float(total_repos))*100.0)
+        if (num_in_both / float(total_repos))*100.0 < policy:
+            return "FAILED"
+        else:
+            return "PASSED"
 
-    def pod_test(self):
+    def opssight_pod_coverage(self, policy):
+        dump, err = getSkyfireDump()
+        total_pods = 0
+        total_pods += len(dump["perceptor-kube-report"]["only_kube_pod_names"])
+        total_pods += len(dump["perceptor-kube-report"]["only_perceptor_pod_names"])
+        total_pods += len(dump["perceptor-kube-report"]["both_pod_names"])
+        num_in_both = len(dump["perceptor-kube-report"]["both_pod_names"])
+        logging.debug("OpsSight Pod Coverage: %f", (num_in_both / float(total_pods))*100.0)
+        if (num_in_both / float(total_pods))*100.0 < policy:
+            return "FAILED"
+        else:
+            return "PASSED"
+
+    def hub_image_coverage(self, policy):
+        dump, err = getSkyfireDump()
+        total_images = 0
+        total_images += len(dump["mult-hub-perceptor-report"]["only_hub_image_shas"])
+        total_images += len(dump["mult-hub-perceptor-report"]["only_perceptor_images"])
+        total_images += len(dump["mult-hub-perceptor-report"]["both_images"])
+        num_in_both = len(dump["mult-hub-perceptor-report"]["both_images"])
+        logging.debug("Hub Image Coverage: %f", (num_in_both / float(total_images))*100.0)
+        if (num_in_both / float(total_images))*100.0 < policy:
+            return "FAILED"
+        else:
+            return "PASSED"
+
+    def create_pod_test(self):
         logging.debug("Starting Pod Test")
         k_client = KubeClient(in_cluster=False)
-
-        skyfire_path = "localhost"
 
         # Create a namespace
         namespace = 'bd' 
@@ -129,7 +181,7 @@ class TestSuite:
         test_result = "FAILED"
 
         for i in range(90):
-            dump, err = getSkyfireDump(skyfire_path)
+            dump, err = getSkyfireDump()
             if err is not None:
                 logging.error(err)
                 time.sleep(10)
@@ -156,8 +208,8 @@ class TestSuite:
         return test_result
             
 
-def getSkyfireDump(host_name="localhost"):
-    url = "http://{}:{}/latestreport".format(host_name, 80)
+def getSkyfireDump(host_name="localhost", port=80):
+    url = "http://{}:{}/latestreport".format(host_name, 9092)
     r = requests.get(url)
     if 200 <= r.status_code <= 299:
         logging.debug("Skyfire http Dump Request Status Code: %s - %s", r.status_code, url)
