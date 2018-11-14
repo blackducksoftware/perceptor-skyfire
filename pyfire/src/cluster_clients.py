346c911610d89743e6d0a0235740d178bdfa1002
Michael Hammer <mike.p.hammer@gmail.com>
2018-11-14 15:23:59 -0500

    cleaned up and commented scrape objects

--- pyfire/src/cluster_clients.py
+++ pyfire/src/cluster_clients.py
@@ -7,22 +7,25 @@ from kubernetes import client, config
 import logging 
 from util import *
 
-'''
-Data Scrape Classes
+''' 
+Data Scrape Classes - Data Access Objects for Dumps
 '''
 
+# Data Access Object for Kube Dump
 class KubeScrape:
     def __init__(self, dump={}):
         self.time_stamp = get_current_datetime()
         self.dump = dump
 
+        # Kube Information
         self.pod_names = []
-        self.ns_pod_names = [] # ns = namespace
+        self.ns_pod_names = []     # ns = namespace, <ns>/<pod-name>  (unique to cluster)
         self.uids = []
         self.namespaces = []
         self.container_names = []
-        self.image_repos = []
+        self.image_repos = []      # <repo>:<tag>
 
+        # Data Access by Namespaced Pod Name
         self.ns_pod_name_to_uid = {}
         self.ns_pod_name_to_annotations = {}
         self.ns_pod_name_to_labels = {}
@@ -44,8 +47,10 @@ class KubeScrape:
                 self.container_names.append(container_name)
                 self.image_repos.append(container_data['image'])
 
+        # Remove Duplicates
         self.pod_names = list(set(self.pod_names))
         self.namespaces = list(set(self.namespaces))
+        self.container_names = list(set(self.container_names))
         self.image_repos = list(set(self.image_repos))
 
     def viz_dump(self):
@@ -97,25 +102,37 @@ class KubeScrape:
                 node['children'].append(ns)
             viz_out['children'].append(node)
         return viz_out 
-                
+
+# Data Access Object for Hub Dump
 class HubScrape():
     def __init__(self, dump={}):
         self.time_stamp = get_current_datetime()
         self.dump = dump
         
-        self.project_names = []
-        self.code_loc_shas = []
-
+        # Project Data and Data Access
         self.project_urls = []
-        self.version_urls = []
-        self.code_loc_urls = []
+        self.project_names = []
 
         self.project_url_to_version_urls = {}
 
+        # Version Data and Data Access
+        self.version_urls = []
+
         self.version_url_to_risk_profile = {}
         self.version_url_to_policy_status = {}
+        self.version_url_to_code_loc_urls = {}
+        self.version_url_to_code_loc_shas = {}
 
-        self.code_loc_sha_to_code_location_url = {}
+        # Code Location URL and Data Access
+        self.code_loc_urls = []
+
+        self.code_loc_url_to_code_loc_sha = {}
+        self.code_loc_url_to_scans = {}
+
+        # Code Location Sha and Data Access
+        self.code_loc_shas = []
+
+        self.code_loc_sha_to_code_loc_url = {}
         self.code_loc_sha_to_scans = {}
 
         self.code_loc_sha_to_project_urls = {}
@@ -126,75 +143,114 @@ class HubScrape():
     def load_dump(self, dump):
         if dump == {}:
             return 
+        # Parse Project URL Data
         for project_url, project_data in dump.items():
+
             self.project_names.append(project_data['name'])
             self.project_urls.append(project_url)
+
+            # Parse Version URL Data
             for version_url, version_data in project_data['versions'].items():
                 if project_url not in self.project_url_to_version_urls:
                     self.project_url_to_version_urls[project_url] = []
                 self.project_url_to_version_urls[project_url].append(version_url)
+
                 self.version_urls.append(version_url)
                 self.version_url_to_risk_profile[version_url] = version_data["riskProfile"]
                 self.version_url_to_policy_status[version_url] = version_data["policy-status"]
+
+                # Partse Code Loc URL and Code Loc Sha Data
                 for code_loc_url, code_loc_data in version_data['codelocations'].items():
                     self.code_loc_urls.append(code_loc_url)
-                    sha = code_loc_data['sha']
-                    self.code_loc_shas.append(sha)
-                    self.code_loc_sha_to_code_location_url[sha] = code_loc_url 
-                    self.code_loc_sha_to_scans[sha] = code_loc_data['scans']
 
-                    if sha not in self.code_loc_sha_to_version_urls:
-                        self.code_loc_sha_to_version_urls[sha] = []
-                    self.code_loc_sha_to_version_urls[sha].append(version_url)
-
-                    if sha not in self.code_loc_sha_to_project_urls:
-                        self.code_loc_sha_to_project_urls[sha] = []
-                    if project_url not in self.code_loc_sha_to_project_urls[sha]:
-                        self.code_loc_sha_to_project_urls[sha].append(project_url)
+                    # Parse Code Loc URL Data
+                    if version_url not in self.version_url_to_code_loc_urls:
+                        self.version_url_to_code_loc_urls[version_url] = []
+                    self.version_url_to_code_loc_urls[version_url].append(code_loc_url)
                     
+                    code_loc_sha = code_loc_data['sha']
+
+                    if version_url not in self.version_url_to_code_loc_shas:
+                        self.version_url_to_code_loc_shas[version_url] = []
+                    self.version_url_to_code_loc_shas[version_url].append(code_loc_sha)
+
+                    self.code_loc_url_to_code_loc_sha[code_loc_url] = code_loc_sha  
+                    self.code_loc_url_to_scans[code_loc_url] = code_loc_data['scans']
+
+                    # Parse Code Loc Sha Data
+                    self.code_loc_shas.append(code_loc_sha)
+
+                    self.code_loc_sha_to_code_loc_url[code_loc_sha] = code_loc_url 
+                    self.code_loc_sha_to_scans[code_loc_sha] = code_loc_data['scans']
+
+                    if code_loc_sha not in self.code_loc_sha_to_project_urls: # Code Loc can be in multiple Projects
+                        self.code_loc_sha_to_project_urls[code_loc_sha] = []
+                    if project_url not in self.code_loc_sha_to_project_urls[code_loc_sha]:
+                        self.code_loc_sha_to_project_urls[code_loc_sha].append(project_url)
+
+                    if code_loc_sha not in self.code_loc_sha_to_version_urls: # Code Loc can be in multiple Versions
+                        self.code_loc_sha_to_version_urls[code_loc_sha] = []
+                    self.code_loc_sha_to_version_urls[code_loc_sha].append(version_url)
+
+        # Remove Duplicates
+        self.project_names = list(set(self.project_names))
         self.code_loc_shas = list(set(self.code_loc_shas))
 
+
+# Data Access Object for Perceptor Dump
 class PerceptorScrape:
     def __init__(self, dump={}):
         self.time_stamp = get_current_datetime()
         self.dump = dump
 
-        self.hub_names = []
-        self.hub_shas = []
-        self.hub_name_to_shas = {}
+        # Hub Section Data
+        self.hub_hosts = []
+        self.hub_code_loc_shas = []
 
+        self.hub_host_to_code_loc_shas = {}
+        self.hub_host_to_code_locs = {}
+
+        # Pod Section Data
         self.pod_names = []
-        self.ns_pod_names = [] # ns = namespace
-        self.pod_uids = []
-        self.pod_image_shas = []
-        self.pod_container_names = []
-        self.pod_image_repos = []
+        self.ns_pod_names = []        # ns = namespace, <ns>/<pod-name>, unique to cluster
         self.pod_namespaces = []
+        self.pod_uids = []
+        self.pod_container_names = []
+        self.pod_image_repos = []     # <repo>:<tag>
+        self.pod_image_shas = []
+        
         self.ns_pod_name_to_containers = {}
 
+        # Image Section Data
         self.image_shas = []
         self.image_repos = []
         self.image_code_loc_shas = []
         self.image_code_loc_uids = []
+
         self.image_sha_to_risk_profile = {}
         self.image_sha_to_policy_status = {}
         self.image_sha_to_scans = {}
-        self.image_sha_to_respositories = {}
+        self.image_sha_to_repos = {}
 
+        # Scan Queue Section Data
         self.scan_queue_shas = []
 
         self.load_dump(dump)
 
     def load_dump(self, dump):
-        for hub_name, hub_data in dump["Hubs"].items():
-            self.hub_names.append(hub_name)
-            shas = list(hub_data["CodeLocations"].keys())
-            self.hub_name_to_shas[hub_name] = shas 
-            self.hub_shas.extend(shas)
-        self.hub_shas = list(set(self.hub_shas))
+        # Load the Hub Section
+        for hub_host, hub_data in dump["Hubs"].items():
+            self.hub_hosts.append(hub_host)
+            code_loc_shas = list(hub_data["CodeLocations"].keys())
+            self.hub_host_to_code_loc_shas[hub_host] = code_loc_shas
+            self.hub_code_loc_shas.extend(code_loc_shas)
+            self.hub_host_to_code_locs[hub_host] = hub_data["CodeLocations"]
+        # Remove duplicates
+        self.hub_shas = list(set(self.hub_shas)) 
 
+        # Load the Pod Section
         for pod_name, pod_data in dump["CoreModel"]["Pods"].items():
-            self.ns_pod_names.append(pod_name)
+            self.ns_pod_names.append(pod_name) 
             self.pod_names.append(pod_data['Name'])
             self.pod_namespaces.append(pod_data["Namespace"])
             self.pod_uids.append(pod_data['UID'])
@@ -203,8 +259,14 @@ class PerceptorScrape:
                 self.pod_container_names.append(container["Name"])
                 self.pod_image_repos.append("{}:{}".format(container["Image"]["Repository"],container["Image"]["Tag"]))
                 self.pod_image_shas.append(container["Image"]["Sha"])
-            self.pod_namespaces = list(set(self.pod_namespaces))
+        # Remove duplicates
+        self.pod_names = list(set(self.pod_names))
+        self.pod_namespaces = list(set(self.pod_namespaces))
+        self.pod_container_names = list(set(self.pod_container_names))
+        self.pod_image_repos = list(set(self.pod_image_repos)) 
+        self.pod_image_shas = list(set(self.pod_image_shas))
 
+        # Load the Images Section
         for image_sha, image_data in dump["CoreModel"]["Images"].items():
             self.image_shas.append(image_sha)
             if image_data["ScanResults"] is not None:
@@ -213,11 +275,12 @@ class PerceptorScrape:
                 self.image_sha_to_scans[image_sha] = image_data["ScanResults"]["ScanSummaries"] 
                 self.image_code_loc_shas.append(image_data["ScanResults"]["CodeLocationName"]) 
                 self.image_code_loc_uids.append(image_data["ScanResults"]["CodeLocationURL"].split(":")[2])
-            self.image_sha_to_respositories[image_sha] = []
+            self.image_sha_to_repos[image_sha] = []
             for repo in image_data["RepoTags"]:
-                self.image_sha_to_respositories[image_sha].append(repo["Repository"]) 
+                self.image_sha_to_repos[image_sha].append(repo["Repository"]) 
                 self.image_repos.append(repo["Repository"])
 
+        # Load the Scan Queue Section
         for queue_item in dump["CoreModel"]["ImageScanQueue"]:
             self.scan_queue_shas.append(queue_item["Key"])
 
--- pyfire/src/reports.py
+++ pyfire/src/reports.py
@@ -15,46 +15,52 @@ class KubeReport:
     def __init__(self, scrape):
         self.scrape = scrape 
 
+        # Basic Kube Metrics
         self.num_namespaces = 0
         self.num_pods = 0
         self.num_containers = 0
         self.num_images = 0
 
-        self.num_pods_fully_labeled = 0
-        self.num_pods_partially_labeled = 0
-        self.num_pods_fully_annotated = 0 
-        self.num_pods_partially_annotated = 0
-
+        # Pod Label Metrics
         self.pods_fully_labeled = []
         self.pods_partially_labeled = []
+        self.pods_note_labeled = []
+        self.pods_full_label_coverage = 0
+        self.pods_partial_label_coverage = 0
+
+        # Pod Annotation Metrics
         self.pods_fully_annotated = []
         self.pods_partially_annotated = []
+        self.pods_not_annotated = []
+        self.pods_full_annotation_coverage = 0
+        self.pods_partial_annotation_coverage = 0
 
         self.parse_scrape(scrape)
 
     def parse_scrape(self, scrape):
         if scrape is None:
             return 
+        # Check Basic Kube Metrics
         self.num_namespaces = len(scrape.namespaces)
         self.num_pods = len(scrape.uids)
         self.num_containers = len(scrape.ns_pod_name_to_containers.values())
         self.num_images = len(scrape.image_repos)
 
+        # Check Labels
         self.pods_fully_labeled = [pod_name for pod_name in scrape.ns_pod_names if self.has_all_labels(pod_name,scrape)]
         self.pods_partially_labeled = [pod_name for pod_name in scrape.ns_pod_names if self.is_partially_labeled(pod_name,scrape)]
         self.pods_not_labeled = [pod_name for pod_name in scrape.ns_pod_names if self.has_no_labels(pod_name,scrape)]
+        
+        self.pods_full_label_coverage = (len(self.pods_fully_labeled) / float(self.num_pods)) * 100
+        self.pods_partial_label_coverage = (len(self.pods_partially_labeled) / float(self.num_pods)) * 100
 
+        # Check Annotations
         self.pods_fully_annotated = [pod_name for pod_name in scrape.ns_pod_names if self.has_all_annotations(pod_name,scrape)]
         self.pods_partially_annotated = [pod_name for pod_name in scrape.ns_pod_names if self.is_partially_annotated(pod_name,scrape)]
         self.pods_not_annotated = [pod_name for pod_name in scrape.ns_pod_names if self.has_no_annotations(pod_name,scrape)]
-
-        self.num_pods_fully_labeled = len(self.pods_fully_labeled)
-        self.num_pods_partially_labeled = len(self.pods_partially_labeled)
-        self.pods_not_labeled = len(self.pods_not_labeled)
-
-        self.num_pods_fully_annotated = len(self.pods_fully_annotated)
-        self.num_pods_partially_annotated = len(self.pods_partially_annotated)
-        self.pods_not_annotated = len(self.pods_not_annotated)
+        
+        self.pods_full_annotation_coverage = (len(self.pods_fully_annotated) / float(self.num_pods)) * 100
+        self.pods_partial_annotation_coverage = (len(self.pods_partially_annotated) / float(self.num_pods)) * 100
 
     def get_opssight_labels(self, pod_name, scrape):
         expected = set(podformat.get_all_labels(len(scrape.container_names)))
@@ -103,18 +109,20 @@ class HubReport:
     def __init__(self, scrape):
         self.scrapes = scrape 
 
+        # Basic Hub Metrics
         self.num_projects = 0
         self.num_versions = 0
         self.num_code_locs = 0
         self.num_code_loc_shas = 0
 
+        # Project Version Metrics
         self.num_projects_with_one_version = 0
         self.num_projects_with_multiple_versions = 0
         self.num_projects_with_no_versions = 0
         
+        # Code Location Metrics
         self.num_code_locations_with_scans = 0
         self.num_code_locations_with_no_scans = 0
-
         self.num_code_loc_in_multiple_projects = 0
         self.num_code_loc_in_multiple_versions = 0
 
@@ -161,16 +169,19 @@ class PerceptorReport:
     def __init__(self, scrape):
         self.scrape = scrape 
 
+        # Hub Section Metrics
         self.hubs = []
-
         self.num_hubs = 0
         self.num_images_in_hubs = 0
 
+        # Pod Section Metrics
         self.num_pods = 0
         self.num_containers = 0
 
+        # Image Section Metrics
         self.num_images_in_multiple_containers = 0
 
+        # Queue Section Metrics
         self.num_code_loc_shas_in_queue = 0
 
         self.parse_scrape(scrape)
@@ -201,11 +212,13 @@ class PerceptorKubeReport:
         self.only_kube_repos = set()
         self.only_perceptor_repos = set()
         self.both_repos = set()
+        self.repo_coverage = 0
 
         # Compare Pods
         self.only_kube_pod_names = set()
         self.only_perceptor_pod_names = set()
         self.both_pod_names = set()
+        self.pod_coverage = 0
 
         self.parse_scrapes(perceptor_scrape, kube_scrape)
 
@@ -219,6 +232,13 @@ class PerceptorKubeReport:
         self.only_perceptor_repos = perceptor_repos.difference(kube_repos)
         self.both_repos = kube_repos.intersection(perceptor_repos)
 
+        total_repos = 0
+        total_repos += len(self.only_kube_repos)
+        total_repos += len(self.only_perceptor_repos)
+        total_repos += len(self.both_repos)
+        num_in_both = len(self.both_repos)
+        self.repo_coverage = (num_in_both / float(total_repos))*100.0
+
         # Compare Pods
         kube_pods = set(kube_scrape.ns_pod_names)
         perceptor_pods = set(perceptor_scrape.ns_pod_names)
@@ -226,12 +246,20 @@ class PerceptorKubeReport:
         self.only_perceptor_pod_names = perceptor_pods.difference(kube_pods)
         self.both_pod_names = kube_pods.intersection(perceptor_pods)
 
+        total_pods = 0
+        total_pods += len(self.only_kube_pod_names)
+        total_pods += len(self.only_perceptor_pod_names)
+        total_pods += len(self.both_pod_names)
+        num_in_both = len(self.both_pod_names)
+        self.pod_coverage = (num_in_both / float(total_pods))*100.0
+
 class HubPerceptorReport:
     def __init__(self, hub_scrapes, perceptor_scrape):
         # Compare Images
         self.only_hub_image_shas = set()
         self.only_perceptor_images = set()
         self.both_images = set()
+        self.image_coverage = 0
 
         self.parse_scrape(hub_scrapes, perceptor_scrape)
     
@@ -246,5 +274,12 @@ class HubPerceptorReport:
         self.only_hub_image_shas = hub_image_shas.difference(perceptor_image_shas)
         self.only_perceptor_images = perceptor_image_shas.difference(hub_image_shas)
         self.both_images = hub_image_shas.intersection(perceptor_image_shas)
+
+        total_images = 0
+        total_images += len(self.only_hub_image_shas)
+        total_images += len(self.only_perceptor_images)
+        total_images += len(self.both_images)
+        num_in_both = len(self.both_images)
+        self.image_coverage = (num_in_both / float(total_images))*100.0
 
 
